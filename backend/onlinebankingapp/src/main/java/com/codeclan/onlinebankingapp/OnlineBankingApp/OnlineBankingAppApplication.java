9923075c2a3499aed6fc48b68a411b83e85d6d91
RobinBailey84 <robinbailey52@hotmail.com>
2019-02-28 16:21:14 +0000

    initial commit

---
+++ backend/onlinebankingapp/src/main/java/com/codeclan/onlinebankingapp/OnlineBankingApp/OnlineBankingAppApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.onlinebankingapp.OnlineBankingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBankingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBankingAppApplication.class, args);
+	}
+
+}
