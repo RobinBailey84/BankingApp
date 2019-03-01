dcff12df027881ac9ad58c0208e1f2fb591e5180
Minal <minalsakriya@hotmail.com>
2019-03-01 14:52:20 +0000

    Customer data added in models and repository

--- backend/onlinebankingapp/src/main/java/com/codeclan/onlinebankingapp/OnlineBankingApp/repository/CustomerRepository.java
+++ backend/onlinebankingapp/src/main/java/com/codeclan/onlinebankingapp/OnlineBankingApp/repository/CustomerRepository.java
@@ -1,4 +1,9 @@
 package com.codeclan.onlinebankingapp.OnlineBankingApp.repository;
 
-public class CustomerRepository {
+import com.codeclan.onlinebankingapp.OnlineBankingApp.models.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository <Customer, Long>{
 }
